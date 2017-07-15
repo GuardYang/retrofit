cbfdfe9b91fa2c6e3707d56db3b9d795cae4f7e1
ysr_feng <1282855229@qq.com>
2017-07-15 08:25:38 +0800

    及时查询接口

--- src/main/java/com/ysr/rf/retrofit/APIService.java
+++ src/main/java/com/ysr/rf/retrofit/APIService.java
@@ -1,9 +1,10 @@
 package com.ysr.rf.retrofit;
 
+import com.ysr.rf.bean.RequestShipperName;
 import com.ysr.rf.bean.ResultDetail;
 import retrofit2.Call;
-import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 /**
@@ -14,11 +15,23 @@ public interface APIService {
      * 单号识别
      */
     @Headers("Content-Type:application/x-www-form-urlencoded")
-    @GET("/Ebusiness/EbusinessOrderHandle.aspx")
-    Call<ResultDetail> searchData(@Query("RequestData") String RequestData,
-                                  @Query("EBusinessID") int EBusinessID,
-                                  @Query("RequestType") int RequestType,
-                                  @Query("DataType") int DataType,
-                                  @Query("DataSign") String DataSign
+    @POST("/Ebusiness/EbusinessOrderHandle.aspx")
+    Call<RequestShipperName> searchData(@Query("RequestData") String RequestData,
+                                        @Query("EBusinessID") int EBusinessID,
+                                        @Query("RequestType") int RequestType,
+                                        @Query("DataType") int DataType,
+                                        @Query("DataSign") String DataSign
+    );
+
+    /**
+     * 即时查询
+     */
+    @Headers("Content-Type:application/x-www-form-urlencoded")
+    @POST("/Ebusiness/EbusinessOrderHandle.aspx")
+    Call<ResultDetail> searchDetailsData(@Query("RequestData") String RequestData,
+                                         @Query("EBusinessID") int EBusinessID,
+                                         @Query("RequestType") int RequestType,
+                                         @Query("DataType") int DataType,
+                                         @Query("DataSign") String DataSign
     );
 }
